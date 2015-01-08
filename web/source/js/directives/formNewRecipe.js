347f44c2d0f4a4f07543c8376785f386e12fd8e9
Philip Schweiger <pschwei1@gmail.com>
2015-01-08 09:39:49 -0500

    Redirect to recipe display after adding a recipe

--- web/source/js/directives/formNewRecipe.js
+++ web/source/js/directives/formNewRecipe.js
@@ -3,12 +3,16 @@
 
   angular.module('ziplist.directives').directive('formNewRecipe', [
     'Recipes',
-    function(Recipes) {
+    '$location',
+    '$log',
+    function(Recipes, $location, $log) {
       return {
         restrict: 'A',
-        link: function(scope, element) {
+        link: function($scope, element) {
           element.on('submit', function() {
-            Recipes.save(scope.recipe);
+            Recipes.save($scope.recipe).then(function redirect(data) {
+              $location.path('/recipes/' + data._id);
+            }, $log.error);
           });
         }
       };
