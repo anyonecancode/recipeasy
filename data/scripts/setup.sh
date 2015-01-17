cf96e2b6038400f8225354813bb8a2e1374e6c92
Philip Schweiger <pschwei1@gmail.com>
2015-01-17 13:53:09 -0500

    Elasticsearch river to couchdb working

--- data/scripts/setup.sh
+++ data/scripts/setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
 DBHOST=admin:pass@db:5984
-
-curl -X PUT $DBHOST/recipes
+SEARCHOST=search:9200
+curl -X PUT $DBHOST/recipes -H "Accept: application/json" -H "Content-type: application/json"
 curl -X PUT $DBHOST/_users/org.couchdb.user:ziplist -H "Accept: application/json" -H "Content-type: application/json" -d '{"name": "ziplist", "password": "pass", "roles": [], "type": "user"}'
 curl -X PUT $DBHOST/recipes/_security -H "Accept: application/json" -H "Content-type: application/json" -d '{"members": {"names": ["ziplist"]}}'
-curl -X PUT $DBHOST/recipes/_design/by_title -H "Accept: application/json" -H "Content-type: application/json" -d '{"_id":"_design/by_title","language":"javascript","views":{"ByTitle":{"map":"function(doc) {if (doc.title) {emit(doc.title, doc.title);}}"}}}'
+curl -X PUT $DBHOST/recipes/_design/by_title -H "Accept: application/json" -H "Content-type: application/json" -d "@/app/scripts/design_doc.json"
+curl -X PUT $SEARCHOST/_river/recipes/_meta -H "Accept: application/json" -H "Content-type: application/json" -d "@/app/scripts/couchdb_river.json"
