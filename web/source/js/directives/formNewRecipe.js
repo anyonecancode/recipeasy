02ced0b674a6ffeb04c3664db1dae1a1571dfec5
Philip Schweiger <pschwei1@gmail.com>
2015-07-30 09:21:30 -0400

    Fix scoping issue on recipe detail view
    Nesting the formNewRecipe directive inside the recipeDetail directive
    caused the previously set $scope.recipe to persist, meaning every time
    you added a new recipe the details from the previous one were in the form.
    
    Reverting that nesting to fix this.

--- web/source/js/directives/formNewRecipe.js
+++ web/source/js/directives/formNewRecipe.js
@@ -9,6 +9,13 @@
     function(Recipes, $routeParams, $location, $log) {
       return {
         restrict: 'A',
+        controller: function($scope) {
+          if ($routeParams.id) {
+            Recipes.get($routeParams.id).then(function setRecipeScope(data) {
+              $scope.recipe = data;
+            }, $log.error);
+          }
+        },
         link: function($scope, element) {
           element.on('submit', function() {
             Recipes.save($scope.recipe).then(function redirect(data) {
